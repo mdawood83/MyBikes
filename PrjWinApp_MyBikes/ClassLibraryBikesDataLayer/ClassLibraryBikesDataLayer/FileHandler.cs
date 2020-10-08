9ae4db2f24588b7c8f901b3f3de4c9063d516bc4
mdawood83 <71352908+mdawood83@users.noreply.github.com>
2020-10-08 12:35:10 -0400

    Add files via upload

---
+++ PrjWinApp_MyBikes/ClassLibraryBikesDataLayer/ClassLibraryBikesDataLayer/FileHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using System.Xml.Serialization;
+
+using ClassLibraryBikesBusLayer;
+
+
+namespace ClassLibraryBikesDataLayer
+{
+    public class FileHandler
+    {
+        public static String binFilePath = @"..\..\data\bike.ser";
+        public static String xmlFilePath = @"..\..\data\myBike.xml";
+
+        //***************** BINARY FILE ***************//
+        //public static void WriteToBinaryFile(List<Bike> list)
+        //{
+        //    FileStream fs = new FileStream(binFilePath, FileMode.OpenOrCreate, FileAccess.Write);
+        //    BinaryFormatter bf = new BinaryFormatter();
+
+        //    bf.Serialize(fs, list);
+
+        //    fs.Close();
+        //}
+        //public static List<Bike> ReadFromBinary()
+        //{
+        //    List<Bike> list = new List<Bike>();
+        //    if (File.Exists(binFilePath))
+        //    {
+        //        FileStream fs = new FileStream(binFilePath, FileMode.Open, FileAccess.Read);
+        //        BinaryFormatter bf = new BinaryFormatter();
+
+        //        list = (List<Bike>)bf.Deserialize(fs);
+
+        //        fs.Close();
+        //    }
+        //    return list;
+        //}
+
+        //*********************** XML FILE ********************//
+        public static void WriteToXmlFile(List<Bike> list)
+        {
+            XmlWriter writer = XmlWriter.Create(xmlFilePath);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Bike>));
+
+            serializer.Serialize(writer, list);
+            writer.Close();
+        }
+
+        public static List<Bike> ReadFromXmlFile()
+        {
+            List<Bike> list;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Bike>));
+
+            StreamReader reader = new StreamReader(xmlFilePath);
+
+            list = (List<Bike>)xmlSerializer.Deserialize(reader);
+            
+            reader.Close();
+            return list;
+        }
+    }
+}
